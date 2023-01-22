da3ba4c45da69e883a20c0b7aad10b3da168f150
Mirza Arslan <60809539+Mirza57@users.noreply.github.com>
2023-01-22 05:10:52 +0500

    Add files via upload

---
+++ Management_Project/Management_Project/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorldNamespace;
+
+namespace Management_Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Country countryOneInstance = new Country();
+           // Country countryTwoInstance = new Country();
+
+            CountryLanguages countryLanguages = new CountryLanguages();
+            countryLanguages.FirstLanguage = Languages.English.ToString();
+            countryLanguages.SecondLanguage = Languages.Dutch.ToString();
+
+           // countryOneInstance.CountryLanguages= countryLanguages;
+
+            countryOneInstance.PlanetName = "Earth";
+            countryOneInstance.CountryName = "Pakistan";
+
+            countryOneInstance.Currencies = Currencies.PKR;
+            countryOneInstance.Continents = Continents.Asia;
+
+            countryOneInstance.SayHi();
+            countryOneInstance.SayHi(countryOneInstance.CountryName);
+
+            Console.WriteLine($"Planet is {countryOneInstance.PlanetName} and country is {countryOneInstance.CountryName} and the currency whick will work there is {countryOneInstance.Currencies}");
+            
+            
+
+        }
+    }
+
+}
